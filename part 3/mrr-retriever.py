c16f603d3258c94a12b28a5db9bee85ad963a8dc
geffeh <34681985+geffeh@users.noreply.github.com>
2020-05-17 13:57:10 +0800

    Update mrr-retriever.py

--- part 3/mrr-retriever.py
+++ part 3/mrr-retriever.py
@@ -39,26 +39,35 @@ def main():
     start_date = '2020-01-01' #change as needed format (yyyy-mm-dd)
     end_date = '2020-04-30' #change as needed (yyyy-mm-dd)
     
+    #Actual API call with promise convert
     result = chartmogul.Metrics.mrr(
         config,
         start_date=start_date,
         end_date=end_date,
         interval=interval).get()
-    #Format the data
-    df = mrr_cleaner(result, df, interval)
+    #Format the data and sorts oldest to newest
+    df = mrr_cleaner(result, df, interval).sort_values(by=['end_date'])
     #Save the data into a csv
     df.to_csv('mrr_data.csv',index=False)
     
-    print('\033[4m\033[1mTotal MRR for Q1 2019\033[0m\n') #escape characters for bold and underline printing
+    print('\033[4m\033[1mTotal MRR for Q1 2019\033[0m\n') #escape characters for bold printing
     
     #This printing method only works if interval is month 
     if interval == 'month':
         for i in range(len(df)):
-            print('{}:  \t{:.2f} USD'.format(datetime.datetime.strptime(df['end_date'][i],'%Y-%m-%d').strftime('%B'), df['mrr'][i]/100)) #Prints and converts MRR from cents to USD
+            mrr = df['mrr'][i]/100
+            if i == 0:
+                change = ' - '
+            else:
+                change = ((mrr-prev)/prev *100).round(1)
+            #Prints and converts MRR from cents to USD
+            print('{}:  \t{:.2f} USD ({}%)'.format(datetime.datetime.strptime(df['end_date'][i],'%Y-%m-%d').strftime('%B'), mrr, change))
+            #This is for percent changed
+            prev = mrr
     
     #Plotting
-    ax = df[['end_date', 'mrr']].plot(x='end_date', linestyle='-', marker='o',colormap='hsv',title = 'mrr movement') # Line Graph
-    df[['end_date', 'mrr_positive','mrr_negative']].plot(x='end_date', kind='bar',stacked = True, ax=ax) # Stacked bar
+    ax = df[['end_date', 'mrr']].plot(x='end_date', linestyle='-', marker='o',colormap='hsv',title = 'mrr movement') #Line graph
+    df[['end_date', 'mrr_positive','mrr_negative']].plot(x='end_date', kind='bar',stacked = True, ax=ax) #Stacked bar with shared axis
 
 if __name__ == "__main__":
     main()
